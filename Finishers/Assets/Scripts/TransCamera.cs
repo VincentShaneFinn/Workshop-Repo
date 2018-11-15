de418fd0686e753feb73d1fc3eb2980bc8e0e2d6
whoops <sl3329@drexel.edu>
2018-11-15 12:23:06 -0500

    transparence

---
+++ Finishers/Assets/Scripts/TransCamera.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TransCamera : MonoBehaviour
+{
+    public float alpha = 0;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    private void OnTriggerEnter(Collider collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            if ((collision.gameObject.gameObject.GetComponent<TransEnemy>()) != null)
+            {
+                TransEnemy e = collision.gameObject.GetComponent<TransEnemy>();
+                e.maketransparent(alpha);
+            }
+        }
+    }
+    private void OnTriggerExit(Collider collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            if ((collision.gameObject.gameObject.GetComponent<TransEnemy>()) != null)
+            {
+                TransEnemy e = collision.gameObject.GetComponent<TransEnemy>();
+                e.stoptransparent();
+            }
+        }
+    }
+}
---
+++ Finishers/Assets/Scripts/TransEnemy.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TransEnemy : MonoBehaviour {
+    private Renderer[] renderers;
+	// Use this for initialization
+	void Start () {
+        renderers = GetComponentsInChildren<Renderer>(true);
+        stoptransparent();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    public void maketransparent(float t) {
+        
+        foreach(Renderer r in renderers) {
+            Color c = r.material.color;
+            c.a = t;
+            r.material.color = c;
+        }
+    }
+    public void stoptransparent() {
+        foreach (Renderer r in renderers)
+        {
+            Color c = r.material.color;
+            c.a = 1f;
+            r.material.color = c;
+        }
+    }
+}
